361c02e2c64e2b45c3977bbdfa3d25547bfbb0a3
Misc <Miscx@live.com>
2014-03-02 18:37:00 +0800

    Init repository

---
+++ Ms/Ms.Entity/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ms.Entity
+{
+    public class User
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; }
+        /// <summary>
+        /// User identity
+        /// </summary>
+        public string UserIdentity { get; set; }
+        /// <summary>
+        /// Created
+        /// </summary>
+        public DateTime Created { get; set; }
+        /// <summary>
+        /// Modified
+        /// </summary>
+        public DateTime Modified { get; set; }
+        /// <summary>
+        /// Deleted
+        /// </summary>
+        public bool Deleted { get; set; }
+    }
+}
